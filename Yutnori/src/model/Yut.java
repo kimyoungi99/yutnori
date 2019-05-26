82198ddb0989a6608f60ad7c49e3d53f48a36f80
kimyoungi99 <kimyoungi99@naver.com>
2019-05-27 01:37:21 +0900

    yut implementation

--- Yutnori/src/model/Yut.java
+++ Yutnori/src/model/Yut.java
@@ -1,5 +1,24 @@
 package model;
+import java.util.Random;
+
 
 public class Yut {
-
+	
+	private boolean isBackDo;
+	private Random random = new Random();
+	
+	public Yut(int yutType) {
+		if(yutType == 3)
+			isBackDo = true;
+		else
+			isBackDo = false;
+	}
+	
+	public boolean getRes() {
+		return random.nextBoolean();
+	}
+	
+	public boolean getIsBackDo() {
+		return isBackDo;
+	}
 }
