c871c1bdd58551d192fab221683755a944ca5445
SeongYunKim <cauboy17@naver.com>
2019-06-05 17:19:41 +0900

    Temp Main

--- Yutnori/src/temp/Main.java
+++ Yutnori/src/temp/Main.java
@@ -1,12 +1,29 @@
 package temp;
 
 import controller.Controller;
-import model.Player;
+import model.Cord;
 
 public class Main {
 	public static void main(String[] args) {
+		/*
+		Cord cord = new Cord();
+		cord.setCord(0,  0);
+		cord.transform(5, true);
+		System.out.println(cord.getX() + "," + cord.getY());
+		cord.transform(4,  false);
+		System.out.println(cord.getX() + "," + cord.getY());
+		cord.transform(4, false);
+		System.out.println(cord.getX() + "," + cord.getY());
+		for(int i=0; i<10; i++) {
+			cord.transform(1, false);
+			System.out.println(cord.getX() + "," + cord.getY());
+		}
+		*/
+		System.out.println("Console ON");
+		
 		Controller controller = new Controller();
 		controller.init();
 		controller.setVisible(true);
+		
 	}
 }
