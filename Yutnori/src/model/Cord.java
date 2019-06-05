a0e711e55884a04f4ca16b3a82618915e20af88c
김영기 <kimyoungi99@naver.com>
2019-06-05 17:17:59 +0900

    Distance Function

--- Yutnori/src/model/Cord.java
+++ Yutnori/src/model/Cord.java
@@ -22,7 +22,7 @@ public class Cord {
 		boolean is_done = false;
 		
 		if(delta == -1 && this.y == 0) {
-			if(this.x == 1 && this.x == 2 && this.y == 3) {
+			if(this.x == 1 || this.x == 2 || this.x == 3) {
 				this.x--;
 				this.y = 4;
 			}
@@ -36,7 +36,13 @@ public class Cord {
 			}
 		}
 		else {
-			if(this.y == 0 && this.x == 0 && start) {
+			if(!start) {
+				if(delta == 5)
+					this.setCord(1, 0);
+				else
+					this.y = delta;
+			}
+			else if(this.y == 0 && this.x == 0) {
 				this.x = 999;
 				this.y = 999;
 				is_done = true;
@@ -72,7 +78,21 @@ public class Cord {
 		if(this.x == 4 && this.y == 2)
 			this.x = 5;
 		
-		
 		return is_done;
+	}
+	
+	public int distance(Cord target) {
+		int res;
+		if((this.x == 1 && target.x == 0)||(this.x == 2 && target.x == 1)||(this.x == 3 && target.x == 2)||(this.x == 3 && target.x == 4)||(this.x == 0 && target.x == 5)||(this.x == 0 && target.x == 3))
+			res = 5 - target.y + this.y;
+		else if(this.x == target.x || this.x == 5 && target.x == 4)
+			res = this.y - target.y;
+		else if(this.x == 3 && target.y == 0)
+			res = -1;
+		else if(this.x != target.x)
+			res = this.y - target.y + 1;
+		else
+			res = this.y + 1;
+		return res;
 	}
 }
--- Yutnori/src/temp/Main.java
+++ Yutnori/src/temp/Main.java
@@ -11,11 +11,18 @@ public class Main {
 		//controller.setVisible(true);
 		
 		Cord c = new Cord();
+		Cord t = new Cord();
+		int test = -1;
 		c.setCord(0, 0);
-		c.transform(1, false);
+		t.setCord(0, 0);
+		//c.transform(test, false);
 		for(int i = 0; i < 20; i++) {
-			System.out.printf("%d %d\n", c.getX(), c.getY());
-			c.transform(3, true);
+			System.out.printf("%d %d     %d     %d %d\n", c.getX(), c.getY(), c.distance(t), t.getX(), t.getY());
+			c.transform(test, true);
+			//if(i == 0)
+			//	t.transform(test, false);
+			//else
+			//	t.transform(test, true);
 		}
 		
 	}
